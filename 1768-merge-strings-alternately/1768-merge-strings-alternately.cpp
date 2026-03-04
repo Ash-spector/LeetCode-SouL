eddd519787bdf42b496ddf52a72e7a52c5259d00
Aniket shukla <shuklaaniket090@gmail.com>
2026-03-04 23:25:40 +0530

    Time: 0 ms (100%), Space: 8.3 MB (83.86%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        std::string res="";
+        int n = word1.length();
+        int m = word2.length();
+        int i =0 ,j = 0 ;
+        while (i < n || j < m) {
+         if (i < n){
+            res+= word1[i++];
+         }
+         if (j < m )
+         {
+            res+= word2[j++];
+         }
+
+        }
+
+         return res;
+        }
+    };
+       
