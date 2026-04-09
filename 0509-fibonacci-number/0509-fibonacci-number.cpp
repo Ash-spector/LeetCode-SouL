1c62df9160f41b053e12660735bea141eb813d89
Aniket shukla <shuklaaniket090@gmail.com>
2026-04-09 22:31:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int fib(int n) {
-    if (n <= 1) return n;
+    int F(int n) {
+        if (n <= 1)
+            return n;
 
-    int prev2 = 0; 
-    int prev1 = 1; 
-
-    for (int i = 2; i <= n; i++) {
-        int curr = prev1 + prev2;
-        prev2 = prev1;
-        prev1 = curr;
+        return F(n - 1) + F(n - 2);
     }
 
-    return prev1;
-}
+    int fib(int n) {
+        if (n <= 1)
+            return n;
+        return F(n);
+    }
 };
