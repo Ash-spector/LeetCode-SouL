232cf21f7b91e1a4465e67196c5b435102a9e3f3
Aniket shukla <shuklaaniket090@gmail.com>
2026-08-30 23:59:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (45.1%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int alt = 0;
+        int maxalt = 0;
+
+        for (int i = 0; i < gain.size(); i++)
+        {
+            alt += gain[i];
+            maxalt = max(maxalt, alt);
+        }
+
+        return maxalt;
+    }
+};
