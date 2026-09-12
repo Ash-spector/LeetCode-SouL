de3788087d4caa6e6337a8d531a48ef0ca9b3cb4
Aniket shukla <shuklaaniket090@gmail.com>
2026-09-12 18:05:48 +0530

    Time: 978 ms (47.82%), Space: 356.3 MB (30.43%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -1,92 +1,139 @@
-//donot reach the graph so i have to do this with ai help 
+// //donot reach the graph so i have to do this with ai help 
+// class Solution {
+// public:
+//     struct State {
+//         long long score = 0;
+//         vector<int> indices;
+//     };
+
+//     State better(State a, State b) {
+//         if (a.score != b.score) {
+//             return a.score > b.score ? a : b;
+//         }
+
+//         return lexicographical_compare(
+//             a.indices.begin(), a.indices.end(),
+//             b.indices.begin(), b.indices.end()
+//         ) ? a : b;
+//     }
+
+//     vector<int> maximumWeight(vector<vector<int>>& intervals) {
+//         int n = intervals.size();
+
+//         // Store: {left, right, weight, original index}
+//         vector<array<int, 4>> a;
+
+//         for (int i = 0; i < n; i++) {
+//             a.push_back({
+//                 intervals[i][0],
+//                 intervals[i][1],
+//                 intervals[i][2],
+//                 i
+//             });
+//         }
+
+//         // Sort by starting position
+//         sort(a.begin(), a.end());
+
+//         // Find first interval whose left > current right
+//         vector<int> next(n);
+
+//         for (int i = 0; i < n; i++) {
+//             int r = a[i][1];
+
+//             int lo = i + 1;
+//             int hi = n;
+
+//             while (lo < hi) {
+//                 int mid = lo + (hi - lo) / 2;
+
+//                 if (a[mid][0] > r) {
+//                     hi = mid;
+//                 } else {
+//                     lo = mid + 1;
+//                 }
+//             }
+
+//             next[i] = lo;
+//         }
+
+//         // dp[i][cnt] = best answer starting from i
+//         // when cnt intervals have already been selected
+//         vector<vector<State>> dp(n + 1, vector<State>(5));
+
+//         for (int i = n - 1; i >= 0; i--) {
+
+//             for (int cnt = 0; cnt <= 4; cnt++) {
+
+//                 // Option 1: Skip current interval
+//                 State skip = dp[i + 1][cnt];
+
+//                 State best = skip;
+
+//                 // Option 2: Take current interval
+//                 if (cnt < 4) {
+
+//                     State take = dp[next[i]][cnt + 1];
+
+//                     take.score += a[i][2];
+//                     take.indices.push_back(a[i][3]);
+
+//                     sort(take.indices.begin(), take.indices.end());
+
+//                     best = better(best, take);
+//                 }
+
+//                 dp[i][cnt] = best;
+//             }
+//         }
+
+//         return dp[0][0].indices;
+//     }
+// };
 class Solution {
-public:
-    struct State {
-        long long score = 0;
-        vector<int> indices;
+    using ll = long long;
+
+    struct Node {
+        ll sum = 0;
+        vector<int> idx;
     };
 
-    State better(State a, State b) {
-        if (a.score != b.score) {
-            return a.score > b.score ? a : b;
-        }
-
-        return lexicographical_compare(
-            a.indices.begin(), a.indices.end(),
-            b.indices.begin(), b.indices.end()
-        ) ? a : b;
-    }
-
+public:
     vector<int> maximumWeight(vector<vector<int>>& intervals) {
         int n = intervals.size();
 
-        // Store: {left, right, weight, original index}
         vector<array<int, 4>> a;
+        for (int i = 0; i < n; i++)
+            a.push_back({intervals[i][0], intervals[i][1], intervals[i][2], i});
 
-        for (int i = 0; i < n; i++) {
-            a.push_back({
-                intervals[i][0],
-                intervals[i][1],
-                intervals[i][2],
-                i
-            });
-        }
-
-        // Sort by starting position
         sort(a.begin(), a.end());
 
-        // Find first interval whose left > current right
-        vector<int> next(n);
+        vector<int> starts;
+        for (auto &x : a) starts.push_back(x[0]);
 
-        for (int i = 0; i < n; i++) {
-            int r = a[i][1];
+        vector<vector<Node>> dp(n + 1, vector<Node>(5));
 
-            int lo = i + 1;
-            int hi = n;
-
-            while (lo < hi) {
-                int mid = lo + (hi - lo) / 2;
-
-                if (a[mid][0] > r) {
-                    hi = mid;
-                } else {
-                    lo = mid + 1;
-                }
-            }
-
-            next[i] = lo;
-        }
-
-        // dp[i][cnt] = best answer starting from i
-        // when cnt intervals have already been selected
-        vector<vector<State>> dp(n + 1, vector<State>(5));
+        auto better = [&](Node &A, Node &B) {
+            if (A.sum != B.sum) return A.sum > B.sum;
+            return A.idx < B.idx;
+        };
 
         for (int i = n - 1; i >= 0; i--) {
+            int nxt = upper_bound(starts.begin(), starts.end(), a[i][1]) - starts.begin();
 
-            for (int cnt = 0; cnt <= 4; cnt++) {
+            for (int k = 1; k <= 4; k++) {
+                Node skip = dp[i + 1][k];
+                Node take;
 
-                // Option 1: Skip current interval
-                State skip = dp[i + 1][cnt];
+                take.sum = a[i][2] + dp[nxt][k - 1].sum;
+                take.idx = dp[nxt][k - 1].idx;
+                take.idx.push_back(a[i][3]);
+                sort(take.idx.begin(), take.idx.end());
 
-                State best = skip;
-
-                // Option 2: Take current interval
-                if (cnt < 4) {
-
-                    State take = dp[next[i]][cnt + 1];
-
-                    take.score += a[i][2];
-                    take.indices.push_back(a[i][3]);
-
-                    sort(take.indices.begin(), take.indices.end());
-
-                    best = better(best, take);
-                }
-
-                dp[i][cnt] = best;
+                dp[i][k] = better(take, skip) ? take : skip;
             }
         }
 
-        return dp[0][0].indices;
+        return dp[0][4].idx;
     }
 };
